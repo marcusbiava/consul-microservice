c531b23c6b4ea4c8be243c1ff2494227be51fb0c
Marcus Venicius Nunes Biava <marcusbiava@gmail.com>
2022-02-21 10:30:02 -0300

    init envia-numeros

---
+++ envia-numeros/src/main/java/com/biava/EnviaNumerosApplication.java
@@ -0,0 +1,13 @@
+package com.biava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviaNumerosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnviaNumerosApplication.class, args);
+	}
+
+}
---
+++ envia-numeros/src/test/java/com/biava/EnviaNumerosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviaNumerosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
