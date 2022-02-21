a63cb6d1ecb798002b4f6035ca01f93ef4e35314
Marcus Venicius Nunes Biava <marcusbiava@gmail.com>
2022-02-21 10:37:27 -0300

    criado end-point no home para retornar o nome da applicação

--- envia-numeros/src/main/java/com/biava/EnviaNumerosApplication.java
+++ envia-numeros/src/main/java/com/biava/EnviaNumerosApplication.java
@@ -1,13 +1,25 @@
 package com.biava;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class EnviaNumerosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EnviaNumerosApplication.class, args);
-	}
+  @Value("${spring.application.name}")
+  private String appName;
+
+  public static void main(String[] args) {
+    SpringApplication.run(EnviaNumerosApplication.class, args);
+  }
+
+  @GetMapping("/")
+  public String hello() {
+    return appName;
+  }
 
 }
